e8ba1f8d978513ff84bc0eb7a5f9619461c027dd
William Joye <wjoye@cfa.harvard.edu>
2017-01-27 17:58:00 +0000

    Fixed pkgIndex file

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,11 @@
+# pkgIndex.tcl to use tkcon as a package via 'package require tkcon'
 #
-# Tcl package index file
+# 'tkcon show' will do all that is necessary to display tkcon
 #
-package ifneeded tkcon 2.7 \
-    [list load [file join $dir ] tkcon]
+# Defaults to:
+#  * the main interp as the "slave"
+#  * hiding tkcon when you click in the titlebar [X]
+#  * using '.tkcon' as the root toplevel
+#  * not displaying itself at 'package require' time
+#
+package ifneeded tkcon 2.7 [list source [file join $dir tkcon.tcl]]
